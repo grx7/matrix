2b863a640fe5136a6b5ccb1cd099d51faa143c04
Oliver Lumby <me@oliverlumby.com>
2017-01-13 23:37:27 +0100

    update gitignore and remove ignored items from git

--- Pods/Target Support Files/Pods-MatrixAppTests/Pods-MatrixAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MatrixAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_MatrixAppTests
-@end
--- Pods/Target Support Files/Pods-MatrixAppUITests/Pods-MatrixAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MatrixAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MatrixAppUITests
-@end
