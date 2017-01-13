0e9f414c2290ff2e44c463c17e1635ca50a5ad94
Oliver Lumby <me@oliverlumby.com>
2017-01-13 13:44:22 +0100

    more work on authentication view

---
+++ MatrixApp/Classes/BottomBorderTextField.swift
@@ -0,0 +1,44 @@
+//
+//  BottomBorderTextField.swift
+//  MatrixApp
+//
+//  Created by Oliver Lumby on 13/01/2017.
+//  Copyright © 2017 Oliver Lumby. All rights reserved.
+//
+
+import UIKit
+
+class BottomBorderTextField: UITextField {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.init(white: 1, alpha: 0.85).cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+        
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+        
+        self.attributedPlaceholder = NSAttributedString(string: self.placeholder!, attributes: [NSForegroundColorAttributeName : UIColor.init(white: 1, alpha: 0.7)])
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 6, y: bounds.origin.y + 6, width: bounds.size.width - 12, height: bounds.size.height - 12);
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return self.textRect(forBounds: bounds);
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- MatrixApp/Controllers/AuthViewController.swift
+++ MatrixApp/Controllers/AuthViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class AuthViewController: UIViewController {
     
-    @IBOutlet var advancedView: UIView?
-    @IBOutlet var advancedButton: UIButton?
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var usernameField: BottomBorderTextField!
+    @IBOutlet weak var passwordField: BottomBorderTextField!
+    @IBOutlet weak var homeServerField: BottomBorderTextField!
+    @IBOutlet weak var identityServerField: BottomBorderTextField!
+    @IBOutlet weak var advancedView: UIView!
+    @IBOutlet weak var advancedButton: UIButton!
+    @IBOutlet weak var advancedHeight: NSLayoutConstraint!
+    
+    let defaultAdvancedHeight: CGFloat = 176.0
+    weak var activeField: UITextField?
+    
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -19,6 +29,10 @@ class AuthViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardDidShow(notification:)), name: .UIKeyboardDidShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: .UIKeyboardWillHide, object: nil)
         
         if let button = self.advancedButton {
             self.toggleAdvancedOptions(sender: button)
@@ -37,22 +51,48 @@ class AuthViewController: UIViewController {
             view.isHidden = !view.isHidden
             
             if view.isHidden {
+                self.advancedHeight.constant = 0
                 button.setTitle("Show Advanced", for: .normal)
             } else {
+                self.advancedHeight.constant = self.defaultAdvancedHeight
                 button.setTitle("Hide Advanced", for: .normal)
             }
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func keyboardDidShow(notification: NSNotification) {
+        if let activeField = self.activeField, let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            
+            print("got here")
+            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+            self.scrollView.contentInset = contentInsets
+            self.scrollView.scrollIndicatorInsets = contentInsets
+            var aRect = self.view.frame
+            aRect.size.height -= keyboardSize.size.height
+            if (!aRect.contains(activeField.frame.origin)) {
+                self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+            }
+        }
     }
-    */
+    
+    func keyboardWillBeHidden(notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+    }
 
+}
+
+//MARK: - UITextFieldDelegate
+
+extension AuthViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.activeField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.activeField = nil
+    }
+    
 }
