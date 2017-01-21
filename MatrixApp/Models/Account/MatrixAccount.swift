ae028a20faef290e89e75bc2072117883f809f44
Oliver Lumby <me@oliverlumby.com>
2017-01-22 00:57:39 +0100

    tidy up MatrixAccount class

--- MatrixApp/Models/Account/MatrixAccount.swift
+++ MatrixApp/Models/Account/MatrixAccount.swift
@@ -18,6 +18,16 @@ class MatrixAccount: NSManagedObject {
     var restClient: MXRestClient = MXRestClient()
     var session: MXSession = MXSession()
     
+    @NSManaged public var deviceId: String?
+    @NSManaged public var homeServer: String?
+    @NSManaged public var identityServer: String?
+    @NSManaged public var userId: String?
+    @NSManaged public var username: String?
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MatrixAccount> {
+        return NSFetchRequest<MatrixAccount>(entityName: "MatrixAccount");
+    }
+    
     var token: String? {
         let keychain = Keychain(service: Constants.service)
         
@@ -41,14 +51,18 @@ class MatrixAccount: NSManagedObject {
             self.session = MXSession(matrixRestClient: restClient)
         }
     }
+
+}
+
+//MARK: - Static methods
+
+extension MatrixAccount {
     
     static func accounts() -> [MatrixAccount] {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "MatrixAccount")
-        
         do {
-            let results = try context.fetch(fetchRequest)
+            let results = try context.fetch(self.fetchRequest())
             
             return results as! [MatrixAccount]
         } catch let error {
@@ -65,7 +79,7 @@ class MatrixAccount: NSManagedObject {
     static func saveAccount(username: String, password: String, homeServer: String, identityServer: String, success: @escaping ((MatrixAccount) -> ()), failure: @escaping ((Error) -> ())) {
         
         MatrixAccount.authenticateUser(username: username, password: password, homeServer: homeServer, success: { (credentials) in
-
+            
             let keychain = Keychain(service: Constants.service)
             let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
             let entity = NSEntityDescription.entity(forEntityName: "MatrixAccount", in: context)
@@ -90,7 +104,7 @@ class MatrixAccount: NSManagedObject {
         }) { (error) in
             failure(error)
         }
-
+        
     }
     
     static func authenticateUser(username: String, password: String, homeServer: String, success: @escaping ((MXCredentials) -> ()), failure: @escaping ((Error) -> ())) {
@@ -115,53 +129,5 @@ class MatrixAccount: NSManagedObject {
             }
         }
     }
-    
-//    var credentials: MXCredentials = MXCredentials()
-//    var restClient: MXRestClient = MXRestClient()
-//    var session: MXSession = MXSession()
-//    
-//    init(credentials: MXCredentials) {
-//        self.credentials = credentials
-//        self.restClient = MXRestClient(credentials: self.credentials, andOnUnrecognizedCertificateBlock: nil)
-//        self.session = MXSession(matrixRestClient: restClient)
-//    }
-//    
-//    static func authenticateUser(username: String, password: String, homeServer: String, success: @escaping ((MXCredentials) -> ()), failure: @escaping ((Error) -> ())) {
-//        let parameters: [AnyHashable: Any] = [
-//            "type": kMXLoginFlowTypePassword,
-//            "user": username,
-//            "password": password
-//        ]
-//        
-//        let restClient = MXRestClient(homeServer: homeServer, andOnUnrecognizedCertificateBlock: nil)!
-//        
-//        restClient.login(parameters, success: { (response) in
-//            if let accessToken = response?["access_token"] as? String, let matrixId = response?["user_id"] as? String, let deviceId = response?["device_id"] as? String, let homeServer = response?["home_server"] as? String {
-//                if let credentials = MXCredentials(homeServer: homeServer, userId: matrixId, accessToken: accessToken) {
-//                    credentials.deviceId = deviceId
-//                    success(credentials)
-//                }
-//            }
-//        }) { (error) in
-//            if error != nil {
-//                failure(error!)
-//            }
-//        }
-//    }
-    
-}
-
-
-extension MatrixAccount {
-    
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<MatrixAccount> {
-        return NSFetchRequest<MatrixAccount>(entityName: "MatrixAccount");
-    }
-    
-    @NSManaged public var deviceId: String?
-    @NSManaged public var homeServer: String?
-    @NSManaged public var identityServer: String?
-    @NSManaged public var userId: String?
-    @NSManaged public var username: String?
 
 }
